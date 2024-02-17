bf81b4cf7fab1a6026e2845f5b70306dcbfb7ee6
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-17 18:46:57 -0300

    Update maker-simpcard.js

--- plugins/maker-simpcard.js
+++ plugins/maker-simpcard.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/simpcard', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '*¡WTF ERES SIMP* 😆', m);
+  }), 'error.png', '*¡WTF VOCÊ JÁ É SIMP* 😆', m);
 };
 handler.help = ['simpcard'];
 handler.tags = ['maker'];
